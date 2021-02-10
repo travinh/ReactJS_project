bcea1ac6b3b06faab43fd59bfa34febf352c8bc4
travinh <vinh.v.tran@ucdenver.edu>
2021-02-10 16:36:56 -0500

    add navbtn and navbtnlink css and index

--- react-nails-app/src/components/Navbar/NavbarElements.js
+++ react-nails-app/src/components/Navbar/NavbarElements.js
@@ -32,7 +32,7 @@ export const NavbarContainer = styled.div`
 `
 
 export const NavLogo = styled(LinkR)`
-    color: red;
+    color: #fff;
     justify-self: flex-start;
     cursor: pointer;
     font-size: 1.5rem;
@@ -54,6 +54,7 @@ export const MobileIcon = styled.div`
         transform: translate(-100%, 60%);
         font-size: 1.8rem;
         cursor: pointer;
+        color: #fff
 
     }
 `
@@ -74,7 +75,7 @@ export const NavItem = styled.li`
     height: 80px;
 `
 
-export const NavLinks = styled(Links)`
+export const NavLinks = styled(LinkS)`
     color: #fff;
     display: flex;
     align-items: center;
@@ -88,6 +89,34 @@ export const NavLinks = styled(Links)`
     }
 `
 
+export const NavBtn = styled.nav`
+    display: flex;
+    align-items: center;
+    
+    @media screen and (max-width: 768px){
+        display: none;
+    }
+`
+
+export const NavBtnLink = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2 ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        trasition: all 0.2s ease-in-out;
+        background: #fff;
+        color: #010606;
+    }
+`
 
 
 
