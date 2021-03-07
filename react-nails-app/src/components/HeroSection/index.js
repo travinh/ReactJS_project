1c1fdbf3453cf6f5fee2fcfa29036e754bd63c8c
travinh <vinh.v.tran@ucdenver.edu>
2021-03-06 20:55:13 -0500

    add video and changesome info

--- react-nails-app/src/components/HeroSection/index.js
+++ react-nails-app/src/components/HeroSection/index.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg} from './HeroElements'
+import React, {useState} from 'react';
+import Video from '../../videos/nails.mp4';
+import {Button} from '../ButtonElement';
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
+
 
 const HeroSection = () => {
+
+    const [hover,setHover] = useState(false)
+
+    const onHover = () =>{
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer id="home">
             <HeroBg>
@@ -15,7 +24,9 @@ const HeroSection = () => {
                 </HeroP>
             </HeroContent>
             <HeroBtnWrapper>
-                <Button to="signup">Get started { hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+                <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    Get started { hover ? <ArrowForward/> : <ArrowRight/>}
+                </Button>
             </HeroBtnWrapper>
         </HeroContainer>
     )
