708c4ad34528ea6ae63c24fa241a4c4800545849
travinh <vinh.v.tran@ucdenver.edu>
2021-03-08 14:08:58 -0500

    add HerroBtnWrapper

--- react-nails-app/src/components/HeroSection/index.js
+++ react-nails-app/src/components/HeroSection/index.js
@@ -24,7 +24,7 @@ const HeroSection = () => {
                 </HeroP>
             </HeroContent>
             <HeroBtnWrapper>
-                <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark="true">
                     Get started { hover ? <ArrowForward/> : <ArrowRight/>}
                 </Button>
             </HeroBtnWrapper>
